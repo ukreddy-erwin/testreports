cf2eeb3d350dd1bf5f479b18bc592f025b43314c
UDay Kiran Reddy <ukreddy@erwin.com>
2020-08-12 22:48:24 +0530

    added xml entry creation and deletion

--- helper_functoins.py
+++ helper_functoins.py
@@ -18,8 +18,8 @@ def validate(date_text):
     except ValueError:
         return False
 
-#https://youtu.be/9DYYKSYtMuw
-def updateXML(filename):
+
+def updateXMLGEO(filename):
     tree=obj.ElementTree(file=filename)
     root = tree.getroot()
 
@@ -37,5 +37,36 @@ def updateXML(filename):
     with open(filename,"wb") as fileupdate:
         tree.write(fileupdate)
 
+#https://youtu.be/9DYYKSYtMuw
+#https://docs.python.org/2/library/xml.etree.elementtree.html
+def updateXMLACT(filename):
+    tree=obj.ElementTree(file=filename)
+    root = tree.getroot()
+    for item in root.iter("item"):
+        child = item.find('Username')
+        lc,bn = child.text.split(",")
+        lc = lc.replace("LC:","")
+        bn = bn.replace("BN:","")
+        #print(lc,bn)
+        item.remove(child)
+        new_child1 = obj.SubElement(item, 'license')
+        new_child1.text = lc
+        new_child2 = obj.SubElement(item, 'buildnumber')
+        new_child2.text = bn
+        StartDate = item.find('StartDate')
+        StartDate.text = str(mariadb_date_format(StartDate.text))
+        EndDate = item.find('EndDate')
+        EndDate.text = str(mariadb_date_format(EndDate.text))
+        Created = item.find('Created')
+        Created.text = str(mariadb_date_format(Created.text))
+        Modified = item.find('Modified')
+        Modified.text = str(mariadb_date_format(Modified.text))
+    
+    tree=obj.ElementTree(root)
+
+    with open(filename,"wb") as fileupdate:
+        tree.write(fileupdate)
+
 if __name__=="__main__":
-    updateXML(r"C:\users\UdayKiranReddy\Desktop\Customer4.xml")
+    updateXMLACT(r"C:\Users\UdayKiranReddy\Downloads\ACT_7095_5539_8_1_2020_8_31_2020.XML")
+    updateXMLGEO(r"C:\Users\UdayKiranReddy\Downloads\GEO_7095_5539_8_1_2020_8_31_2020.XML")
