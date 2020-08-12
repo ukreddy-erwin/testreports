9d647378b5ac96e596cd380bb01a54f5078715a8
UDay Kiran Reddy <ukreddy@erwin.com>
2020-08-12 18:30:13 +0530

    Dateconversion to xml updation added

--- helper_functoins.py
+++ helper_functoins.py
@@ -1,8 +1,41 @@
+import xml.etree.ElementTree as obj
+import datetime
+
 def mariadb_date_format(sDate):
     sDate = sDate.strip()
-    import datetime
-    #sDate = '3/1/2020 1:04:46 PM'
-    d = datetime.datetime.strptime(sDate,'%d/%m/%Y %I:%M:%S %p')
-    return d
+    if validate(sDate):
+        #sDate = '3/1/2020 1:04:46 PM'
+        d = datetime.datetime.strptime(sDate,'%d/%m/%Y %I:%M:%S %p')
+        return d
+    else:
+        return sDate
 
-print(mariadb_date_format('3/1/2020 1:04:46 PM'))
+
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%d/%m/%Y %I:%M:%S %p')
+        return True
+    except ValueError:
+        return False
+
+#https://youtu.be/9DYYKSYtMuw
+def updateXML(filename):
+    tree=obj.ElementTree(file=filename)
+    root = tree.getroot()
+
+    for dates in root.iter("Date"):
+        dates.text = str(mariadb_date_format(dates.text))
+
+    for dates in root.iter("Created"):
+        dates.text = str(mariadb_date_format(dates.text))
+
+    for dates in root.iter("Modified"):
+        dates.text = str(mariadb_date_format(dates.text))
+
+    tree=obj.ElementTree(root)
+
+    with open(filename,"wb") as fileupdate:
+        tree.write(fileupdate)
+
+if __name__=="__main__":
+    updateXML(r"C:\users\UdayKiranReddy\Desktop\Customer4.xml")
